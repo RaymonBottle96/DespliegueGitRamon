7866b097299463acba531aaecd600e19faca51c6
Rus <colibri@ejemplo.com>
2019-02-26 16:09:58 +0100

    modificacion rama nueva

---
+++ comentario.php
@@ -0,0 +1,46 @@
+    <div class="cajaFormulario">
+        <div class="titulo">
+            <label>Comentarios</label>
+        </div>
+            <ul class="listaComentarios">
+                <?php
+                    $conexion = mysqli_connect("localhost","root","","comentario");
+
+                    if(isset($_POST['submit']))  {
+                        $nombre = $_POST['name'];
+                        $comentario = $_POST['comentario'];
+                        $lenguaje = $_POST['lenguaje'];
+
+                        //preparacion stmt//
+                        $stmt = $conexion->prepare("INSERT INTO tablacomentario(nombre, comentario,lenguaje) VALUES (?, ?, ?)");
+                        $stmt->bind_param("sss", $nombre, $comentario, $lenguaje);
+                        $stmt->execute();
+                        $stmt->close();
+                    }
+
+                    if($resultado = $conexion->query("SELECT * from tablacomentario")) {
+                        if (mysqli_num_rows($resultado) > 0) {
+                            while($fila = mysqli_fetch_assoc($resultado)) {
+                                echo '<li>';
+                                    echo '<div>';
+                                        echo "<p class='nombre'>".$fila['nombre']."</p><p> Tipo de lenguaje: ".$fila ['lenguaje']."</p><p>".$fila['comentario']."</p>";
+
+                                    echo '</div>';
+                                echo '</li>';
+                            }
+                        }
+                    }else {
+                        echo "Error en la sentencia: (" . $conexion->errno . ") " . $conexion->error; 
+                    }
+                    mysqli_close($conexion);
+                ?>
+            </ul>
+            <form class="form-inline" action="#" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeHolder="Nombre usuario" required>
+                    <input type="text" class="form-control" name="lenguaje" placeHolder="Lenguaje utilizado">
+                    <textarea class="form-control" maxlength="128" name="comentario" placeHolder="Máximo 150 caracteres"></textarea>
+                    <input type="submit" class="botonAnadir" name="submit" value="Añadir">
+                </div>
+            </form>
+        </div>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <title>Practica Despliegue Lenguajes de programacion</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" >
         <link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" href="comentario.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     </head>
